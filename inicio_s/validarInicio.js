667861563b7027d63cbb78ded3c08ee82186f0fb
Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>
2022-10-18 16:42:59 -0500

    Add files via upload
    Signed-off-by: Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>

---
+++ inicio_s/validarInicio.js
@@ -0,0 +1,42 @@
+var attempt=10;
+
+function validateInicio(){
+
+    var password=document.getElementById("password").value;
+
+    if(password =="12345678"){
+
+        window.location="P_secun/inicio.html";
+
+        return true;
+    
+    }
+    else{
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'Lo siento  :(',
+            text: 'No perteneces a este coro'
+          })
+    }
+
+}
+
+function Recordatorio1(){
+        
+    Swal.fire({
+        //icon: 'error',
+        title: 'MISA COMPLETADA',
+        //text: 'ㅤㅤFecha: 12 DE NOVIEMBRE 2022ㅤㅤㅤㅤㅤㅤHorario: 13:00 ㅤㅤㅤㅤㅤㅤLugar: El sauz Bajo'
+      })
+}
+
+function Recordatorio2(){
+        
+    Swal.fire({
+        //icon: 'error',
+        title: 'Misa de boda',
+        text: 'ㅤㅤFecha: 12 DE NOVIEMBRE 2022ㅤㅤㅤㅤㅤㅤHorario: 13:00 ㅤㅤㅤㅤㅤㅤLugar: El sauz Bajo'
+      })
+}
+
