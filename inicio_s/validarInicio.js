b9b2c780d7faaa9c5d20c2cd71f99aaa7915e1f4
Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>
2022-10-19 15:16:20 -0500

    Update validarInicio.js
    Signed-off-by: Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>

--- inicio_s/validarInicio.js
+++ inicio_s/validarInicio.js
@@ -36,7 +36,7 @@ function Recordatorio2(){
     Swal.fire({
         //icon: 'error',
         title: 'Misa de boda',
-        text: 'Fecha: 12 DE NOVIEMBRE 2022ㅤㅤㅤㅤㅤㅤHorario: 13:00ㅤㅤ ㅤㅤㅤㅤLugar: El sauz Bajo'
+        text: 'Fecha: 12 DE NOVIEMBRE 2022ㅤㅤㅤㅤㅤㅤㅤㅤㅤHorario: 13:00ㅤㅤㅤㅤㅤ ㅤㅤㅤㅤLugar: El sauz Bajo'
       })
 }
 
