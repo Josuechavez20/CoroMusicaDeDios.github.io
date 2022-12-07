266a1c0eed879ba81bfbe7d25c05cffe914a88cf
Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>
2022-12-06 23:39:33 -0600

    Add files via upload
    Signed-off-by: Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>

---
+++ inicio_s/validarPresupuesto.js
@@ -0,0 +1,199 @@
+var attempt=10;
+
+function validatePresuP(){
+
+    var usuar=document.getElementById("usuar").value;
+    var password=document.getElementById("password").value;
+
+    if(usuar=="JosueC" && password=="20290237"){
+        
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'JosueC'
+          })
+        
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    
+    }if(usuar=="MiguelR" && password=="20290238"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'MiguelR'
+          })
+
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="LupillaH" && password=="20290239"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'LupillaH'
+          })
+          window.location="../P_secun/Presupuesto.html";
+        return true;
+    
+    }if(usuar=="LupitaR" && password=="20290230"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'LupitaR'
+          })
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="YoselinR" && password=="20290241"){
+
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'YoselinR'
+          })
+
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    
+    }if(usuar=="DannaG" && password=="20290242"){
+         
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'DannaG'
+          })
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="MarianaR" && password=="20290243"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'MarianaR'
+          })
+        
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    
+    }if(usuar=="VeroP" && password=="20290244"){  
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'VeroP'
+          })
+
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="MagdaR" && password=="20290245"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'MagdaR'
+          })
+        
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    
+    }if(usuar=="JuanaR" && password=="20290246"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'JuanaR'
+          })
+
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="PakoR" && password=="20290247"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'PakoR'
+          })
+        
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    
+    }if(usuar=="RosyG" && password=="20290248"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'RosyG'
+          })
+
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="ElsiV" && password=="20290249"){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'USUARIO CORRECTO',
+            text: 'ElsiV'
+          })
+        
+          window.location="../P_secun/Presupuesto.html";
+
+        return true;
+    }
+    if(usuar=="PerlaM" && password=="20290250"){
+
+      Swal.fire({
+        icon: 'error',
+        title: 'LO SIENTO :(',
+        text: 'No puedes entrar a este apartado'
+      })
+         
+
+        return true;
+    }
+    if(usuar=="MiguelS" && password=="20290251"){
+
+      Swal.fire({
+        icon: 'error',
+        title: 'LO SIENTO :(',
+        text: 'No puedes entrar a este apartado'
+      })
+        
+        return true;
+    }
+    if(usuar=="Invitado" && password=="12345678"){
+
+      Swal.fire({
+        icon: 'error',
+        title: 'LO SIENTO :(',
+        text: 'Tu invitacion no te permiten entrar a este apartado'
+      })
+  }
+    else{
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'Algo salio mal',
+            text: 'Intenta de nuevo'
+          })
+    }
+}
