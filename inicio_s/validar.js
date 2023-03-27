7af3e21004419c81849ac63f2a05a82fc35c1202
Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>
2023-03-26 21:03:33 -0600

    Update validar.js
    Signed-off-by: Josuechavez20 <110755721+Josuechavez20@users.noreply.github.com>

--- inicio_s/validar.js
+++ inicio_s/validar.js
@@ -5,12 +5,12 @@ function validate(){
     var usuar=document.getElementById("usuar").value;
     var password=document.getElementById("password").value;
 
-    if(usuar=="JosueC" && password=="20290237"){
+    if(password=="24681012"){
         
         Swal.fire({
             icon: 'success',
-            title: 'USUARIO CORRECTO',
-            text: 'JosueC'
+            title: 'CONTRASEÑA CORRECTO',
+            text: 'BIENVENIDO'
           })
         
           window.location="https://drive.google.com/drive/folders/1LAgfabdNTv1pYnBtrQ_pC34j43QO6wT5?usp=sharing";
